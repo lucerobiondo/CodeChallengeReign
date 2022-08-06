d733a353ddade95eb99b052bca59df093d279bd2
Nicolas Lucero <lucerobiondo@gmail.com>
2022-08-05 23:26:35 -0300

    nicolas-001-fixes. *Fix warning and dependencies to deployment without errors.

--- frontend/cypress/support/component.js
+++ frontend/cypress/support/component.js
@@ -1,4 +1,15 @@
 /// <reference types='cypress' />
 import { mount } from 'cypress/react'
+import { MemoryRouter } from 'react-router-dom'
 
 Cypress.Commands.add('mount', mount)
+
+Cypress.Commands.add('clickLink', (label) => {
+    cy.get('a').contains(label).click()
+})
+
+Cypress.Commands.add('mount', (component, options = {}) => {
+  const { routerProps = { initialEntries: ['/'] }, ...mountOptions } = options
+  const wrapped = <MemoryRouter {...routerProps}>{component}</MemoryRouter>
+  return mount(wrapped, mountOptions)
+})
